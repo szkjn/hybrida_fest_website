c0311ac020043b21ec246b4c0f78f6c3af100703
Jun Suzuki <junsuzuki@gmx.fr>
2024-02-18 18:12:56 +0100

    Justified nav bar to top right corner + added debug css classes

--- app/src/lib/components/Header.svelte
+++ app/src/lib/components/Header.svelte
@@ -1,28 +1,27 @@
 <script lang="ts">
-
 </script>
 
 <header>
 	<nav class="f f-a-items-start f-j-c-space-between">
 		<div>
 			<a class="f f-a-items-center" href="/">
-					<h1 class="m-0">HYBRIDA FEST 2024</h1>
-					&nbsp;<span>></span>&nbsp;
-					<h1 class="m-0">JUNE 27—29</h1>
+				<h1 class="m-0">HYBRIDA FEST 2024</h1>
+				&nbsp;<span>></span>&nbsp;
+				<h1 class="m-0">JUNE 27—29</h1>
 			</a>
 		</div>
-		<ul class="f f-d-column f-w-wrap m-0 p-0">
+		<ul class="f f-j-c-space-between f-w-wrap m-0 p-0">
 			<li>
-					<a href="/volunteers">VOLUNTEERS</a>
+				<a href="/volunteers">VOLUNTEERS</a>
 			</li>
 			<li>
-					<a href="/tickets">TICKETS</a>
+				<a href="/tickets">TICKETS</a>
 			</li>
 			<li aria-current="false">
-					<a href="https://www.instagram.com/hybrida.space/">INSTAGRAM</a>
+				<a href="https://www.instagram.com/hybrida.space/" target="_blank">INSTAGRAM</a>
 			</li>
 			<li>
-					<a href="/about">ABOUT</a>
+				<a href="/about">ABOUT</a>
 			</li>
 		</ul>
 	</nav>
@@ -33,32 +32,35 @@
 		position: sticky;
 		top: 13px;
 		z-index: 1000;
-  	width: 100%;
+		width: 100%;
 	}
 	nav > div {
 		padding-right: 10px;
 	}
 	ul {
 		height: 42px;
-    width: 285px;
+		width: 285px;
 		list-style: none;
 	}
-	a, span, h1 {
+	a,
+	span,
+	h1 {
 		font-family: var(--font-secondary);
 		font-size: var(--font-size-2);
-  	text-decoration: none;
+		text-decoration: none;
 		/* TODO: penser à faire fonctionner la classe layer-exclusion-white (voir global.css)*/
-		color: var(--font-color-1); 
+		color: var(--font-color-1);
 	}
-	h1, span {
+	h1,
+	span {
 		font-weight: 500;
 	}
 	a {
 		font-weight: 300;
 	}
 	@media screen and (max-width: 399px) {
-	nav > ul {
-		height: auto;
-	}
+		nav > ul {
+			height: auto;
+		}
 	}
 </style>
