344a8e57895253b55bcc323b20dc71653f2359c3
Jimmy Pez <jim@MBP-de-Jimmy.lan>
2024-02-22 20:23:09 +0100

    drop menu

---
+++ app/src/lib/components/menu/Button.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import gsap from 'gsap';
+
+	const onHoverMenu = () => toggle ? null : gsap.to(".menu-line", {duration: 0.2, text: "1"});
+	const onOutHoverMenu = () => toggle ? null : gsap.to(".menu-line", {duration: 0.2, text: "-"});
+	const onClickMenu = () => {
+		gsap.to(".menu-line", {duration: 0.2, text: "1111"});
+		toggle = !toggle;
+	}
+	export let toggle = false;
+</script>
+
+<button 
+	class="menu-icon" 
+	on:mouseover={onHoverMenu}
+	on:focus={onHoverMenu}
+	on:mouseout={onOutHoverMenu}
+	on:blur={onOutHoverMenu}
+	on:click={onClickMenu}
+>
+	<span class="menu-line">-</span>
+	_
+</button>
+
+
+<style>
+	@media screen and (min-width: 601px) {
+		button {
+			display: none;
+		}
+	}
+	button {
+		z-index: 1000;
+	}
+	.menu-icon {
+		cursor: pointer;	
+		background: none;
+		border: none;
+		color: #fff;
+		font-family: var(--font-secondary);
+		font-size: var(--font-size-3);
+	}
+</style>
---
+++ app/src/lib/components/menu/Drop.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	const onClickItem = () => toggle = false;
+	export let toggle: Boolean;
+</script>
+
+{#if toggle}
+    <div class="f f-d-column f-j-c-center">
+        {#each ['ABOUT', 'TICKETS', 'VOLUNTEERS', 'INSTAGRAM'] as link}
+            <p on:click={onClickItem}>
+                {link}
+            </p>
+        {/each}
+    </div>
+{/if}
+
+<style>
+    div {
+		touch-action: none;
+		overflow-y: hidden;
+		mix-blend-mode: normal;
+		z-index: 999;
+		position: absolute;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+        text-align: center;
+        letter-spacing: 0.15em;
+		font-family: var(--font-secondary);
+		background-color: #FFF;
+    }
+    p {
+        cursor: pointer;
+        width: max-content;
+        margin: 2rem auto;
+		font-size: clamp(1em, 6vw , 2em);
+    }
+	p:nth-child(1), p:nth-child(2){
+		text-decoration: line-through;
+		cursor: none;
+	}
+</style>
