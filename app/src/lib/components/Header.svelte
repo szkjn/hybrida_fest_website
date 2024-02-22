80fc78a0bf43c4e2929fb88804be150e7bea00d9
Jimmy Pez <jim@MBP-de-Jimmy.lan>
2024-02-22 20:23:09 +0100

    drop menu

--- app/src/lib/components/Header.svelte
+++ app/src/lib/components/Header.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
-</script>
+	import gsap from 'gsap';
+	import { TextPlugin } from "gsap/dist/TextPlugin";
+	import { onMount } from 'svelte';
+	import ButtonMenu from '$lib/components/menu/Button.svelte';
+	import DropMenu from '$lib/components/menu/Drop.svelte';
 
+	onMount(() => {
+		let mm = gsap.matchMedia();
+		mm.add("(max-width: 601px)", () => {
+			gsap.registerPlugin(TextPlugin);
+			gsap.timeline({ repeat: -1 })
+			.to(".date", {duration: 2, text: "2024"})
+			.to(".date", {duration: 2, text: "JUNE 27—29"})
+			.play()
+		});
+	});
+	let toggle = false;
+</script>
 <header>
 	<nav class="f f-a-items-start f-j-c-space-between">
 		<div>
 			<a class="f f-a-items-center" href="/">
-				<h1 class="m-0" id="test">HYBRIDA FEST 2024</h1>
-				&nbsp;>&nbsp;
-				<h1 class="m-0">JUNE 27—29</h1>
+				<div class="f hybrida">
+					<h1 class="m-0">HYBRIDA FEST <span class="media-600">2024&nbsp;</span></h1>
+					<h1 class="m-0"><span>>&nbsp;</span><span class="date">JUNE 27—29</span></h1>
+				</div>
+	
 			</a>
 		</div>
+		<ButtonMenu bind:toggle={toggle}/>
 		<ul class="f f-j-c-space-between f-w-wrap m-0 p-0">
 			<li>
 				<a href="/volunteers">VOLUNTEERS</a>
@@ -26,6 +45,7 @@
 		</ul>
 	</nav>
 </header>
+<DropMenu bind:toggle={toggle}/>
 
 <style>
 	header {
@@ -62,9 +82,19 @@
 	.deactivated {
 		text-decoration: line-through;
 	}
-	@media screen and (max-width: 399px) {
+	@media screen and (max-width: 600px) {
 		nav > ul {
-			height: auto;
+			display: none;
+		}
+		nav > div > a{
+			flex-direction: column;
+			align-items: flex-start;
+		}
+		.media-600 {
+			display: none;
+		}
+		.hybrida {
+			flex-direction: column;
 		}
 	}
 </style>
