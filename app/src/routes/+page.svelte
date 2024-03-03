9276caeb16a2847888d1708ef0344c3414b98528
Jun Suzuki <junsuzuki@gmx.fr>
2024-03-03 17:23:31 +0100

    Updated keywords and corresponding thumbnail pics, solves #12

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -8,48 +8,201 @@
 	import people_04 from '$lib/assets/photos/1280p/people_04.jpg';
 	import people_03 from '$lib/assets/photos/1280p/people_03b.jpg';
 
+	// Importing preview thumbnails pictures
+	// Keyword: "Experimental art x music"
+	import thumb_exp_art_01 from '$lib/assets/photos/360p/exp_art_music_01.jpg';
+	import thumb_exp_art_02 from '$lib/assets/photos/360p/exp_art_music_02.jpg';
+	import thumb_exp_art_03 from '$lib/assets/photos/360p/exp_art_music_03.jpg';
+	import thumb_exp_art_04 from '$lib/assets/photos/360p/exp_art_music_04.jpg';
+	import thumb_exp_art_05 from '$lib/assets/photos/360p/exp_art_music_05.jpg';
+	import thumb_exp_art_06 from '$lib/assets/photos/360p/exp_art_music_06.jpg';
+	import thumb_exp_art_07 from '$lib/assets/photos/360p/exp_art_music_07.jpg';
+	import thumb_exp_art_08 from '$lib/assets/photos/360p/exp_art_music_08.jpg';
+	// Keyword: "Swedish forests"
+	import thumb_nature_01 from '$lib/assets/photos/360p/nature_01.jpg';
+	import thumb_nature_02 from '$lib/assets/photos/360p/nature_02.jpg';
+	import thumb_nature_03 from '$lib/assets/photos/360p/nature_03.jpg';
+	import thumb_nature_04 from '$lib/assets/photos/360p/nature_04.jpg';
+	import thumb_nature_05 from '$lib/assets/photos/360p/nature_05.jpg';
+	import thumb_nature_06 from '$lib/assets/photos/360p/nature_06.jpg';
+	import thumb_nature_07 from '$lib/assets/photos/360p/nature_07.jpg';
+	// Keyword: "Exhibition"
+	import thumb_exhib_01 from '$lib/assets/photos/360p/exhib_01.jpg';
+	import thumb_exhib_02 from '$lib/assets/photos/360p/exhib_02.jpg';
+	import thumb_exhib_03 from '$lib/assets/photos/360p/exhib_03.jpg';
+	import thumb_exhib_04 from '$lib/assets/photos/360p/exhib_04.jpg';
+	import thumb_exhib_05 from '$lib/assets/photos/360p/exhib_05.jpg';
+	import thumb_exhib_06 from '$lib/assets/photos/360p/exhib_06.jpg';
+	import thumb_exhib_07 from '$lib/assets/photos/360p/exhib_07.jpg';
+	import thumb_exhib_08 from '$lib/assets/photos/360p/exhib_08.jpg';
+	import thumb_exhib_09 from '$lib/assets/photos/360p/exhib_09.jpg';
+	import thumb_exhib_10 from '$lib/assets/photos/360p/exhib_10.jpg';
+	import thumb_exhib_11 from '$lib/assets/photos/360p/exhib_11.jpg';
+	import thumb_exhib_12 from '$lib/assets/photos/360p/exhib_12.jpg';
+	// Keyword: "ironworks"
+	import thumb_ironworks_01 from '$lib/assets/photos/360p/ironworks_01.jpg';
+	import thumb_ironworks_02 from '$lib/assets/photos/360p/ironworks_02.jpg';
+	import thumb_ironworks_03 from '$lib/assets/photos/360p/ironworks_03.jpg';
+	import thumb_ironworks_04 from '$lib/assets/photos/360p/ironworks_04.jpg';
+	import thumb_ironworks_05 from '$lib/assets/photos/360p/ironworks_05.jpg';
+	import thumb_ironworks_06 from '$lib/assets/photos/360p/ironworks_06.jpg';
+	import thumb_ironworks_07 from '$lib/assets/photos/360p/ironworks_07.jpg';
+	import thumb_ironworks_08 from '$lib/assets/photos/360p/ironworks_08.jpg';
+	// Keyword: "Live performances"
 	import thumb_live_01 from '$lib/assets/photos/360p/live_01.jpg';
 	import thumb_live_02 from '$lib/assets/photos/360p/live_02.jpg';
 	import thumb_live_03 from '$lib/assets/photos/360p/live_03.jpg';
 	import thumb_live_04 from '$lib/assets/photos/360p/live_04.jpg';
-	import thumb_install_01 from '$lib/assets/photos/360p/install_01.jpg';
-	import thumb_install_02 from '$lib/assets/photos/360p/install_02.jpg';
-	import thumb_install_03 from '$lib/assets/photos/360p/install_03.jpg';
-	import thumb_install_04 from '$lib/assets/photos/360p/install_04.jpg';
+	import thumb_live_05 from '$lib/assets/photos/360p/live_05.jpg';
+	import thumb_live_06 from '$lib/assets/photos/360p/live_06.jpg';
+	import thumb_live_07 from '$lib/assets/photos/360p/live_07.jpg';
+	import thumb_live_08 from '$lib/assets/photos/360p/live_08.jpg';
+	// Keyword: "Workshops"
 	import thumb_workshop_01 from '$lib/assets/photos/360p/workshop_01.jpg';
 	import thumb_workshop_02 from '$lib/assets/photos/360p/workshop_02.jpg';
 	import thumb_workshop_03 from '$lib/assets/photos/360p/workshop_03.jpg';
 	import thumb_workshop_04 from '$lib/assets/photos/360p/workshop_04.jpg';
+	import thumb_workshop_05 from '$lib/assets/photos/360p/workshop_05.jpg';
+	import thumb_workshop_06 from '$lib/assets/photos/360p/workshop_06.jpg';
+	// Keyword: "DJ sets"
 	import thumb_dj_01 from '$lib/assets/photos/360p/dj_01.jpg';
 	import thumb_dj_02 from '$lib/assets/photos/360p/dj_02.jpg';
 	import thumb_dj_03 from '$lib/assets/photos/360p/dj_03.jpg';
 	import thumb_dj_04 from '$lib/assets/photos/360p/dj_04.jpg';
-	import thumb_nature_01 from '$lib/assets/photos/360p/nature_01.jpg';
-	import thumb_nature_02 from '$lib/assets/photos/360p/nature_02.jpg';
-	import thumb_nature_03 from '$lib/assets/photos/360p/nature_04.jpg';
-	import thumb_nature_04 from '$lib/assets/photos/360p/nature_05.jpg';
+	import thumb_dj_05 from '$lib/assets/photos/360p/dj_05.jpg';
+	import thumb_dj_06 from '$lib/assets/photos/360p/dj_06.jpg';
+	// Keyword: "meals"
+	import thumb_meals_01 from '$lib/assets/photos/360p/meals_01.jpg';
+	import thumb_meals_02 from '$lib/assets/photos/360p/meals_02.jpg';
+	import thumb_meals_03 from '$lib/assets/photos/360p/meals_03.jpg';
+	import thumb_meals_04 from '$lib/assets/photos/360p/meals_04.jpg';
+	import thumb_meals_05 from '$lib/assets/photos/360p/meals_05.jpg';
+	import thumb_meals_06 from '$lib/assets/photos/360p/meals_06.jpg';
+	import thumb_meals_07 from '$lib/assets/photos/360p/meals_07.jpg';
+	import thumb_meals_08 from '$lib/assets/photos/360p/meals_08.jpg';
+	// Keyword: "sauna"
+	import thumb_sauna_01 from '$lib/assets/photos/360p/sauna_01.jpg';
+	import thumb_sauna_02 from '$lib/assets/photos/360p/sauna_02.jpg';
+	import thumb_sauna_03 from '$lib/assets/photos/360p/sauna_03.jpg';
+	import thumb_sauna_04 from '$lib/assets/photos/360p/sauna_04.jpg';
+	import thumb_sauna_05 from '$lib/assets/photos/360p/sauna_05.jpg';
+	// Keyword: "new friends"
+	import thumb_friends_01 from '$lib/assets/photos/360p/friends_01.jpg';
+	import thumb_friends_02 from '$lib/assets/photos/360p/friends_02.jpg';
+	import thumb_friends_03 from '$lib/assets/photos/360p/friends_03.jpg';
+	import thumb_friends_04 from '$lib/assets/photos/360p/friends_04.jpg';
+	import thumb_friends_05 from '$lib/assets/photos/360p/friends_05.jpg';
+	import thumb_friends_06 from '$lib/assets/photos/360p/friends_06.jpg';
+	import thumb_friends_07 from '$lib/assets/photos/360p/friends_07.jpg';
 
 	import RevealImage from '$lib/components/RevealImage.svelte';
 
-	const live_images = [thumb_live_01, thumb_live_02, thumb_live_03, thumb_live_04];
-	const install_images = [thumb_install_01, thumb_install_02, thumb_install_03, thumb_install_04];
-	const workshop_images = [
+	const thumb_exp_art = [
+		thumb_exp_art_01,
+		thumb_exp_art_02,
+		thumb_exp_art_03,
+		thumb_exp_art_04,
+		thumb_exp_art_05,
+		thumb_exp_art_06,
+		thumb_exp_art_07,
+		thumb_exp_art_08
+	];
+	const thumb_nature = [
+		thumb_nature_01,
+		thumb_nature_02,
+		thumb_nature_03,
+		thumb_nature_04,
+		thumb_nature_05,
+		thumb_nature_06,
+		thumb_nature_07
+	];
+	const thumb_exhib = [
+		thumb_exhib_01,
+		thumb_exhib_02,
+		thumb_exhib_03,
+		thumb_exhib_04,
+		thumb_exhib_05,
+		thumb_exhib_06,
+		thumb_exhib_07,
+		thumb_exhib_08,
+		thumb_exhib_09,
+		thumb_exhib_10,
+		thumb_exhib_11,
+		thumb_exhib_12
+	];
+	const thumb_ironworks = [
+		thumb_ironworks_01,
+		thumb_ironworks_02,
+		thumb_ironworks_03,
+		thumb_ironworks_04,
+		thumb_ironworks_05,
+		thumb_ironworks_06,
+		thumb_ironworks_07,
+		thumb_ironworks_08
+	];
+	const thumb_live = [
+		thumb_live_01,
+		thumb_live_02,
+		thumb_live_03,
+		thumb_live_04,
+		thumb_live_05,
+		thumb_live_06,
+		thumb_live_07,
+		thumb_live_08
+	];
+	const thumb_workshop = [
 		thumb_workshop_01,
 		thumb_workshop_02,
 		thumb_workshop_03,
-		thumb_workshop_04
+		thumb_workshop_04,
+		thumb_workshop_05,
+		thumb_workshop_06
 	];
-	const dj_images = [thumb_dj_01, thumb_dj_02, thumb_dj_03, thumb_dj_04];
-	const nature_images = [thumb_nature_01, thumb_nature_02, thumb_nature_03, thumb_nature_04];
+	const thumb_djset = [
+		thumb_dj_01,
+		thumb_dj_02,
+		thumb_dj_03,
+		thumb_dj_04,
+		thumb_dj_05,
+		thumb_dj_06
+	];
+	const thumb_meals = [
+		thumb_meals_01,
+		thumb_meals_02,
+		thumb_meals_03,
+		thumb_meals_04,
+		thumb_meals_05,
+		thumb_meals_06,
+		thumb_meals_07,
+		thumb_meals_08
+	];
+	const thumb_sauna = [
+		thumb_sauna_01,
+		thumb_sauna_02,
+		thumb_sauna_03,
+		thumb_sauna_04,
+		thumb_sauna_05
+	];
+	const thumb_friends = [
+		thumb_friends_01,
+		thumb_friends_02,
+		thumb_friends_03,
+		thumb_friends_04,
+		thumb_friends_05,
+		thumb_friends_06,
+		thumb_friends_07
+	];
 </script>
 
 <section>
 	<h2 class="m-0">
 		<!-- &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -->
-		Hybrida Fest is an intimate experimental art x music festival in the Swedish
-		<RevealImage keyword="forests" imageUrls={nature_images} />.
-		<br/>
-		<br/>
+		Hybrida Fest is an intimate
+		<RevealImage keyword="experimental arts x music" imageUrls={thumb_exp_art} />festival in the
+		Swedish
+		<RevealImage keyword="forests" imageUrls={thumb_nature} />.
+		<br />
+		<br />
 	</h2>
 </section>
 <section>
@@ -57,14 +210,12 @@
 </section>
 <section>
 	<p class="m-0">
-		<br/>
-		<RevealImage keyword="Residency exhibition" imageUrls={install_images} /> in a rambling ironworks.
-		Alongside
-		<RevealImage keyword="performances" imageUrls={live_images} />,
-		<RevealImage keyword="workshops" imageUrls={workshop_images} />, and <RevealImage
-			keyword="DJ sets"
-			imageUrls={dj_images}
-		/>.
+		<br />
+		<RevealImage keyword="Residency exhibition" imageUrls={thumb_exhib} /> in a rambling
+		<RevealImage keyword="ironworks" imageUrls={thumb_ironworks} />. Alongside
+		<RevealImage keyword="performances" imageUrls={thumb_live} />,
+		<RevealImage keyword="workshops" imageUrls={thumb_workshop} />, and
+		<RevealImage keyword="DJ sets" imageUrls={thumb_djset} />.
 	</p>
 </section>
 <section>
@@ -72,12 +223,15 @@
 </section>
 <section>
 	<p class="m-0">
-		<br/>
-		Eat home cooked meals together. Jump in the lake still sweaty from the sauna. Toast to new
-		friends.
+		<br />
+		Eat home cooked
+		<RevealImage keyword="meals" imageUrls={thumb_meals} /> together. Jump in the lake still sweaty from
+		the
+		<RevealImage keyword="sauna" imageUrls={thumb_sauna} />. Toast to new
+		<RevealImage keyword="friends" imageUrls={thumb_friends} />.
 	</p>
 </section>
-<section class="f f-d-row f-j-c-center inline-photos f-row-gap-15">
+<!-- <section class="f f-d-row f-j-c-center inline-photos f-row-gap-15">
 	<img class="f-g-1" alt="" src={texture_01} />
 	<img class="f-g-1" alt="" src={texture_02} />
 	<img class="f-g-1" alt="" src={live_01} />
@@ -117,10 +271,10 @@
 	/></g
 ></svg
 	>
-</section>
+</section> -->
 <section>
 	<p class="m-0">
-		<br/>
+		<br />
 		Ca. 3 hrs from Gothenburg, Stockholm & Oslo in Värmland.
 	</p>
 </section>
@@ -129,7 +283,7 @@
 </section>
 <section>
 	<p class="m-0">
-		<br/>
+		<br />
 		Come and take part in Hybrida Fest 2024 with us !
 	</p>
 	<div class="m-0 f">
@@ -218,7 +372,7 @@
 	}
 	@media screen and (max-width: 768px) {
 		.inline-photos {
-			display: none
+			display: none;
 		}
 		/* .inline-photos > svg {
 			display: block;
