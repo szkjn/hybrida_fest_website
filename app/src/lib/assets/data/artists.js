7959997ff9165f18c2d7c94b2ac86c8bcbf99619
Jimmy Pez <jim@MBP-de-Jimmy.lan>
2024-04-11 01:11:58 +0200

    component Artist

---
+++ app/src/lib/assets/data/artists.js
@@ -0,0 +1,68 @@
+const artists = [
+    {
+        "alias": "SSTROM",
+        "category": "Live music performance",
+        "Website or relevant representation": "https://soundcloud.com/sstromsound",
+        "Instagram": "https://instagram.com/sstromsstrom",
+        "bio": "As one half of sample twisters SHXCXCHCXSH, SSTROM takes on a more analog approach by manipulating repetitive sequences of hardware...",
+        "project": "For the Fest he will get into dynamics of opposites. Sponged up endless reverbs. Congealed liquids. Intense masses of sequencer motifs carved out by compressors and equalisers to bare intimacy. Beats or no beats."
+    },
+    {
+        "alias": "DJ Seduce",
+        "category": "DJ set",
+        "Website or relevant representation": "https://open.spotify.com/artist/4PXEOCYJCEFoJAqiKkJFX0?si=h-WqmViBSc2lPd0MsXBTRw",
+        "Instagram": "https://instagram.com/djseduce_",
+        "bio": "Sweden based DJ Seduce has quickly become a vitality to the modern rave scene. Nurtured by a lifelong love for music, every shred of influence spills out in her take on contemporary club music...",
+        "project": "DJ Seduce looking forard to delivering one hell of a DJ-set at Hybrida Fest :) and  create a magical energy and experience with the crowd <3"
+    },
+    {
+        "alias": "Tomasz Skibicki",
+        "category": "Residency artist",
+        "Website or relevant representation": "https://www.hoarder-gatherer.org",
+        "Instagram": "https://instagram.com/skiibiickii",
+        "bio": "Over the last years, Tomasz Skibicki have been developing  a workflow that is at once research-based and materially dedicated. In this framework, he produce installations, sculptures and films.",
+        "project": "Tomasz will work on a installation featuring a sculptural arrangement complemented by an audio piece."
+    },
+    {
+        "alias": "Dip Shim",
+        "category": "DJ set",
+        "Website or relevant representation": "https://soundcloud.com/dipshim",
+        "Instagram": "https://www.instagram.com/diego__shim/",
+        "bio": "Dip Shim is a Spanish-born producer who relocated to Malmö some years back and found his calling in the reincarnation of electro through his relentless output of acid-flavored 303 jams...",
+        "project": "He is joining us to share his music, create an immersive experience that inspires people to dance, have fun, and connect through music. And look forward to building connections with the music community that gathers in the forrests of Värmland:)"
+    },
+    {
+        "alias": "Miglė Vyčinaitė",
+        "category": "Residency artist",
+        "Website or relevant representation": "",
+        "Instagram": "https://instagram.com/migimigle",
+        "bio": "Miglė Vyčinaitė is an artist whose work revolves around several central themes, notably the creation of modern myths and magical materialism...",
+        "project": "During her time at Hybrida residency, she plan to develop a video-based installation."
+    },
+    {
+        "alias": "Marus Bø",
+        "category": "DJ set",
+        "Website or relevant representation": "https://soundcloud.com/mariusbo",
+        "Instagram": "https://www.instagram.com/marius___bo/",
+        "bio": "Marius Bø is one of the co-founders of the Oslo-based collective Ute. A Oslo-based record label and event series spawned out of hosting gatherings, deep in the Norwegian forest...",
+        "project": "Marius is super excited to join the intimate gathering, play as well as listening to great music in amazing surroundings."
+    },
+    {
+        "alias": "DJ Selfhelp",
+        "category": "DJ set",
+        "Website or relevant representation": "https://centre-of-nowhere.com/",
+        "Instagram": "https://www.instagram.com/olofbara/",
+        "bio": "Will add in a bit. Lorem Ipsum. Document, participate, host, smile and wiggle. And play a DJ Set.",
+        "project": ""
+    },
+    {
+        "alias": "Toma Kami",
+        "category": "DJ Set",
+        "Website or relevant representation": "https://soundcloud.com/toma-kami",
+        "Instagram": "https://www.instagram.com/toma_kami/",
+        "bio": "\"In recent years Toma Kami has been shaping an eclectic yet recognisable sound with several outings on Bristol's Livity Sound as well as his own imprint mb studio...",
+        "project": ""
+    }
+];
+
+export default artists;
---
+++ app/src/lib/components/Artist.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import ElevenOnHover from '$lib/components/ElevenOnHover.svelte';
+
+	export let imgPath: string;
+	export let artist: any;
+
+	onMount(() => {});
+
+</script>
+
+<div class="f">
+	<img src={imgPath}/>
+	<div class="info">
+		<div class="f f-j-c-space-between f-d-column">
+			<h2>{artist["alias"]}</h2>
+			<h3>{artist["category"]}</h3>
+		</div>
+		<hr />
+		<div>
+			<p>
+				{artist["bio"]}
+			</p>
+		</div>
+		<div class="f">
+			{#if artist["Instagram"]}
+				<a target="_blank" href={artist["Instagram"]}><ElevenOnHover text="INSTAGRAM"></ElevenOnHover></a>
+			{/if}
+			{#if artist["Website or relevant representation"]}
+				<a target="_blank" href={artist["Website or relevant representation"]}><ElevenOnHover text="WEBSITE"></ElevenOnHover></a>
+			{/if}
+		</div>
+	</div>
+</div>
+
+<style>
+	img, .info {
+		margin: 20px;
+	}
+	h2 {
+		font-family: var(--font-secondary);
+		font-size: 20px;
+		margin: 0;
+	}
+	h3 {
+		font-family: var(--font-secondary);
+		text-transform: uppercase;
+		font-weight: 300;
+		margin: 0;
+	}
+	p {
+		font-family: var(--font-primary);
+	}
+	a {
+		margin-right: 10px;
+	}
+</style>
