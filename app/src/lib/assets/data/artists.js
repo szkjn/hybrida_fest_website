693a419429c548500fa7b0d7aca09d95cc0922eb
Jimmy Pez <jim@MBP-de-Jimmy.lan>
2024-04-11 13:25:22 +0200

    fix paragraph bio artist

--- app/src/lib/assets/data/artists.js
+++ app/src/lib/assets/data/artists.js
@@ -6,7 +6,7 @@ const artists = [
         "Website or relevant representation": "https://soundcloud.com/sstromsound",
         "Instagram": "https://instagram.com/sstromsstrom",
         "imgPath": "sstrom.jpg",
-        "bio": "As one half of sample twisters SHXCXCHCXSH, SSTROM takes on a more analog approach by manipulating repetitive sequences of hardware...",
+        "bio": "As one half of sample twisters SHXCXCHCXSH, SSTROM takes on a more analog approach by manipulating repetitive sequences of hardware. Through the years the soundscape has emerged from a distinct hypnotic minimalism into a vast cinematic and more adventurous wilderness. His works on Voam, Rösten and Semantica are characterised by immersion, intense immediacy and spontaneity.",
         "project": "For the Fest he will get into dynamics of opposites. Sponged up endless reverbs. Congealed liquids. Intense masses of sequencer motifs carved out by compressors and equalisers to bare intimacy. Beats or no beats."
     },
     {
@@ -16,7 +16,7 @@ const artists = [
         "Website or relevant representation": "https://open.spotify.com/artist/4PXEOCYJCEFoJAqiKkJFX0?si=h-WqmViBSc2lPd0MsXBTRw",
         "Instagram": "https://instagram.com/djseduce_",
         "imgPath": "dj_seduce.jpg",
-        "bio": "Sweden based DJ Seduce has quickly become a vitality to the modern rave scene. Nurtured by a lifelong love for music, every shred of influence spills out in her take on contemporary club music...",
+        "bio": "Sweden based DJ Seduce has quickly become a vitality to the modern rave scene. Nurtured by a lifelong love for music, every shred of influence spills out in her take on contemporary club music. Breakbeat science is her craft, driven by emotive melodies and well-executed rhythms, effortlessly moving through landscapes of jungle, techno and UK bass.",
         "project": "DJ Seduce looking forard to delivering one hell of a DJ-set at Hybrida Fest :) and  create a magical energy and experience with the crowd <3"
     },
     {
@@ -36,7 +36,7 @@ const artists = [
         "Website or relevant representation": "https://soundcloud.com/dipshim",
         "Instagram": "https://www.instagram.com/diego__shim/",
         "imgPath": "dip_shim.jpg",
-        "bio": "Dip Shim is a Spanish-born producer who relocated to Malmö some years back and found his calling in the reincarnation of electro through his relentless output of acid-flavored 303 jams...",
+        "bio": "Dip Shim is a Spanish-born producer who relocated to Malmö some years back and found his calling in the reincarnation of electro through his relentless output of acid-flavored 303 jams. Founder of Malmø Traxx label, and Malmös vinyl community, Bolero. He’s also known as Digge Shim. Influenced by Dutch West Coast, Detroit Techno, and the Spanish scene, he's released with labels like Gated (UK), SNC Recs.",
         "project": "He is joining us to share his music, create an immersive experience that inspires people to dance, have fun, and connect through music. And look forward to building connections with the music community that gathers in the forrests of Värmland:)"
     },
     {
@@ -46,7 +46,7 @@ const artists = [
         "Website or relevant representation": "",
         "Instagram": "https://instagram.com/migimigle",
         "imgPath": "migle_vycinaite.jpg",
-        "bio": "Miglė Vyčinaitė is an artist whose work revolves around several central themes, notably the creation of modern myths and magical materialism...",
+        "bio": "Miglė Vyčinaitė is an artist whose work revolves around several central themes, notably the creation of modern myths and magical materialism. She develops speculative scenarios intertwined with non-canonical histories. Her work invites us to contemplate the connections between contemporary storytelling and the myths that shape our understanding of the world.",
         "project": "During her time at Hybrida residency, she plan to develop a video-based installation."
     },
     {
@@ -56,7 +56,7 @@ const artists = [
         "Website or relevant representation": "https://soundcloud.com/mariusbo",
         "Instagram": "https://www.instagram.com/marius___bo/",
         "imgPath": "marius_bo.jpg",
-        "bio": "Marius Bø is one of the co-founders of the Oslo-based collective Ute. A Oslo-based record label and event series spawned out of hosting gatherings, deep in the Norwegian forest...",
+        "bio": "Marius Bø is one of the co-founders of the Oslo-based collective Ute. A Oslo-based record label and event series spawned out of hosting gatherings, deep in the Norwegian forest. His sets can be seen as a concoction of psychedelic atmospherics, generating dynamic tensions and sublime releases that work both the body and mind.",
         "project": "Marius is super excited to join the intimate gathering, play as well as listening to great music in amazing surroundings."
     },
     {
@@ -76,7 +76,7 @@ const artists = [
         "Website or relevant representation": "https://soundcloud.com/toma-kami",
         "Instagram": "https://www.instagram.com/toma_kami/",
         "imgPath": "toma_kami.jpg",
-        "bio": "\"In recent years Toma Kami has been shaping an eclectic yet recognisable sound with several outings on Bristol's Livity Sound as well as his own imprint mb studio...",
+        "bio": "In recent years Toma Kami has been shaping an eclectic yet recognisable sound with several outings on Bristol's Livity Sound as well as his own imprint mb studio, with the latter being a home to his Speed Oddity releases, also soon to become a party series in 2024 with a first residency at Helsinki’s Post Bar. A specialisation in rhythmic variety partly informed by his dynamic DJ sets, that have become well known synonyms for wild rides through the likes of heavy bass, dancehall, techno, jungle, electro, rap, and any other style that will match his expression of specifically physical and unusual energy on the dance floor. An approach that has seen him tour the likes of USA and Asia, as well as world renowned clubs like De School and Berghain, and regular slots at festivals like Dekmantel. Through his label mb studio, Toma continues to push his own sound and vision as well as fresh music from exciting new producers from the burgeoning French scene and beyond. ",
         "project": ""
     }
 ];
