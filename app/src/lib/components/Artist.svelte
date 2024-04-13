633f8efa3630d4139d9070e62a05cb18c642af57
Jimmy Pez <jim@MBP-de-Jimmy.lan>
2024-04-13 17:28:01 +0200

    responsivness-mobile

--- app/src/lib/components/Artist.svelte
+++ app/src/lib/components/Artist.svelte
@@ -9,9 +9,9 @@
 	onMount(() => {});
 </script>
 
-<div class="container">
-	<img src={img} id={artist.slug} />
-	<div class="info">
+<div class="container" id={artist.slug}>
+	<img class="img-600more" src={img} />
+	<div class="info f f-d-column">
 		<div class="f f-j-c-space-between f-d-column">
 			<h2>{artist['alias']}</h2>
 			<h3>{artist['category']}</h3>
@@ -29,7 +29,8 @@
 				</p>
 			{/if}
 		</div>
-		<div class="f">
+		<img class="img-600less" src={img} />
+		<div class="link f">
 			{#if artist['Instagram']}
 				<a target="_blank" href={artist['Instagram']} 
 					style="text-decoration-color: {$page.route.id === '/program' ? '#fe9055' : '#ffffff'}"
@@ -47,18 +48,16 @@
 </div>
 
 <style>
-	/* img,
-	.info {
-		margin: 20px;
-	} */
 	.container {
 		display: flex;
 		margin-top: 1rem;
+		scroll-margin-top: 40px;
 	}
-	img {
-		/* width: 33%; */
+	.img-600less {
+		display: none;
 	}
 	.info {
+		width: 100%;
 		margin-left: 1rem;
 	}
 	h2 {
@@ -82,10 +81,45 @@
 	.project {
 		font-style: italic;
 	}
+	hr {
+  		margin-left: 0;
+  		margin-right: 0;
+	}
 
-	@media screen and (max-width: 600px) {
-		img {
+	@media screen and (max-width: 1127px) {
+	/* @media screen and (max-width: 600px) { */
+		.img-600more{
 			display: none;
+		}
+		.img-600less {
+			display: block;
+			width: 100%;
+			align-self: center;
+			margin-bottom: 10px;
+		}
+		.info {
+			margin: 0 0 30px 0;
+		}
+		.link{
+			align-self: center;
+		}
+	}
+	@media screen and (max-width: 1127px) {
+	/* @media screen and (max-width: 600px) { */
+		.img-600more{
+			display: none;
+		}
+		.img-600less {
+			display: block;
+			max-width: 360px;
+			align-self: center;
+			margin-bottom: 10px;
+		}
+		.info {
+			margin: 0 0 30px 0;
+		}
+		.link{
+			align-self: center;
 		}
 	}
 </style>
