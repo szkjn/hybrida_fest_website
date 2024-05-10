2e6d21dbdfe2dc5a48c746d78b63e99111231a3e
Jun Suzuki <junsuzuki@gmx.fr>
2024-05-10 15:39:53 +0200

    Updated footer text and added residency link

--- app/src/lib/components/Footer.svelte
+++ app/src/lib/components/Footer.svelte
@@ -7,17 +7,26 @@
 
 <footer class="f f-j-c-space-between m-0 m-b-2">
 	<div class="top-footer">
-		<u>WITH SUPPORT FROM:</u>
-		<img src={logos_logo} alt="" width="200" id="test" />
-		<img src={karlstads_logo} alt="" width="80" />
-		<img src={sac_logo} alt="" width="150" />
-		<img src={varmland_logo} alt="" width="150" />
+		<div class="top-footer-title">
+			<p class="sponsor-title">
+				Hybrida fest is realised in cooperation by Byalaget Älvan, with generous support from:
+			</p>
+		</div>
+		<div class="logos">
+			<img src={logos_logo} alt="" width="200" id="test" />
+			<img src={karlstads_logo} alt="" width="80" />
+			<img src={sac_logo} alt="" width="150" />
+			<img src={varmland_logo} alt="" width="150" />
+		</div>
 	</div>
 	<div class="low-footer">
-		<p>HOME PAGE PICTURES BY OLOF EKMAN</p>
-		<p>GRAPHIC DESIGN BY BIN KOH</p>
+		<p>HOME PAGE PICTURES -> OLOF EKMAN</p>
+		<p>GRAPHIC DESIGN -> BIN KOH</p>
+		<p>
+			RESIDENCY -> <a href="www.hybrida.space">www.hybrida.space</a>
+		</p>
 		<br />
-		<p>WEBDESIGN BY J&J</p>
+		<p>WEBDESIGN -> J&J</p>
 		<p>©2024 HYBRIDA FEST</p>
 		<p>ALL RIGHTS RESERVED</p>
 	</div>
@@ -32,31 +41,43 @@
 	}
 	.top-footer {
 		display: flex;
-		align-items: center;
-		justify-content: space-between;
+		flex-direction: column;
 		border-top: 1px solid white;
 		margin: 2rem 0;
 		padding-top: 1rem;
+	}
+	.top-footer-title {
+		margin-bottom: 1rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.logos {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 	}
 	.low-footer {
 		display: flex;
 		flex-direction: column;
 	}
-	.low-footer > p {
+	.low-footer > p,
+	.top-footer-title > p {
 		margin: auto;
 	}
 	p,
-	u {
+	a {
 		font-family: var(--font-secondary);
 		font-size: var(--font-size-5);
+		color: white;
 	}
 	@media screen and (max-width: 600px) {
-		.top-footer {
+		.logos {
 			flex-direction: column;
 			gap: 1rem;
 		}
-		p,
-		u {
+
+		p {
 			margin: 1px 0 1px 0;
 		}
 	}
